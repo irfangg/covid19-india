06f761a7041e4664e75eae4eaaedc49bf4631222
irfangg <irfangg66@gmail.com>
2020-03-29 23:26:28 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/covid19-india/precache-manifest.77667957eff72f299b8b69fa70358d8f.js"
+  "/covid19-india/precache-manifest.8d57f7be7272e8a182675dc2f75ced2b.js"
 );
 
 self.addEventListener('message', (event) => {
